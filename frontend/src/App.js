39180ab509fcf02f1621f0fefd5bfe7bfae6476c
Rupesh <dasrupesh2124@gmail.com>
2025-06-20 00:26:07 +0530

    Tested Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
 
 const App = () => {
   return (
     <div>
       <Header title="Images Gallery"/>
+      <Search />
       {/* <h1>Mikel Arteta</h1> */}
     </div>
   );
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Search = () => {
+    return (
+        <Container>
+            <Row>
+                <Col> Search </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Search;
