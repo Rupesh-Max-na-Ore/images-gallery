a0ed1b70a29489beff66359abf6240caa8b2c341
Rupesh <dasrupesh2124@gmail.com>
2025-06-20 01:36:50 +0530

    Uncontrolled Search Component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target[0].value);
+}
 
 const App = () => {
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search />
+      <Search handleSubmit={handleSearchSubmit}/>
       {/* <h1>Mikel Arteta</h1> */}
     </div>
   );
