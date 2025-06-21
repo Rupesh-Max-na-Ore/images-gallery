93a32507b0b184fcb75bd6088ad69095f268762b
Rupesh <dasrupesh2124@gmail.com>
2025-06-21 12:06:03 +0530

    fixed code with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,30 +7,32 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
-  
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-     .then((res) => res.json())
-     .then((data) => {
-      console.log(data); 
-     })
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
 
-     .catch((err) => {
-      console.log(err);
-     })
-  setWord(''); //clear search text after submit
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord(''); //clear search text after submit
+  };
 
   // console.log(word); // see state changes
   // console.log(process.env.REACT_APP_UNSPLASH_KEY);
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue'
+  backgroundColor: 'lightblue',
 };
 
 const Header = ({ title }) => {
