0c871592797580104f6a6d34cfae8146163ba929
Rupesh <dasrupesh2124@gmail.com>
2025-06-30 01:07:21 +0530

    basic images endpoint with get and post method

--- api/main.py
+++ api/main.py
@@ -1,9 +1,12 @@
 import os #to read env vars
 import requests
-from flask import Flask, request
+from flask import Flask, json, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
-from mongo_client import insert_test_document
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -23,8 +26,6 @@ CORS(app)
 # Debug mode ON to auto restart flask app on any code changes in main.py
 app.config["DEBUG"] = DEBUG 
 
-insert_test_document()
-
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
@@ -37,6 +38,21 @@ def new_image():
     
     data = response.json()
     return data
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        # read images from the DBase
+        images = images_collection.find({})
+        return jsonify([img for img in images])
+    
+    if request.method == "POST":
+        # save images int he DBase
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
