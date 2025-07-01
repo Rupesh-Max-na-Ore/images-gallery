92a430822c1e2d3fea52b402e0e06620d89709c3
Rupesh <dasrupesh2124@gmail.com>
2025-07-01 12:51:08 +0530

    Added single image endpoint with delete method

--- api/main.py
+++ api/main.py
@@ -54,5 +54,18 @@ def images():
         inserted_id = result.inserted_id
         return {"inserted_id": inserted_id}
 
+@app.route("/images/<image_id>", methods=["DELETE"])
+def image(image_id):
+    if request.method == "DELETE":
+        # delete image from DBase
+        result = images_collection.delete_one({"_id": image_id})
+        if not result:
+            # Server side error, in case issue w/pyhton or mongodb
+            return {"error": "Image was not deleted. Please try again."}, 500 
+        if result and not result.deleted_count:
+            return {"error": "Image not found"}, 404
+        return {"deleted_id": image_id}
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
